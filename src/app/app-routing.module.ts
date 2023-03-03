ca35b531d675f6b31d4d6d02c041e34ecf2ea8a2
Ethan Farbiash <ethan.farbiash@gmail.com>
2023-03-03 14:29:35 +0200

    Remove alignment page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AlignmentPageComponent } from './modules/alignment/alignment-page.component';
 import { DropDownPageComponent } from './modules/drop-down/drop-down-page.component';
 import { ResizeAndMovePageComponent } from './modules/resize-and-move/resize-and-move-page.component';
 
 const routes: Routes = [
     { path: 'drop-downs', component: DropDownPageComponent },
     { path: 'resize-and-move', component: ResizeAndMovePageComponent },
-    { path: 'alignment', component: AlignmentPageComponent },
     { path: '**', redirectTo: 'drop-downs', pathMatch: 'full' },
 ];
 
--- src/app/modules/alignment/alignment.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ngx-test-alignment',
-    templateUrl: './alignment.component.html',
-    styleUrls: ['./alignment.component.scss']
-})
-export class AlignmentComponent { }
