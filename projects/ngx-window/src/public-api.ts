411d12519969f2d290c84211def2c8bc904fa5e2
Ethan Farbiash <ethan.farbiash@gmail.com>
2023-03-10 19:11:14 +0200

    added missing dependency and small fix

--- projects/ngx-window/src/public-api.ts
+++ projects/ngx-window/src/public-api.ts
@@ -6,5 +6,6 @@ export * from './lib/element-position.service';
 export * from './lib/alignment.service';
 export * from './lib/window.service';
 export * from './lib/window.component';
+export * from './lib/window-closer.directive';
 export * from './lib/window.types';
 export * from './lib/ngx-window.module';
